cfbe951483d0f3bed8c67299e5965a8164b3f86b
Tomas Padilla <padillatomasagustin@gmail.com>
2022-03-31 13:53:44 +0200

    Footer - Comenzamos

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./Footer.sass";
+
+const Footer = () => {
+	return (
+		<footer className="footer">
+			<div className="section">
+				<h2>Footer</h2>
+			</div>
+		</footer>
+	);
+};
+
+export default Footer;

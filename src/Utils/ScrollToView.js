b8e8232bc5ae8831ec5e7658044f378aa492fdd2
Tomas Padilla <padillatomasagustin@gmail.com>
2022-04-10 14:03:23 +0200

    Scrolls DONE

--- src/Utils/ScrollToView.js
+++ src/Utils/ScrollToView.js
@@ -1,5 +1,12 @@
 export default function ScrollToView(scrollToId) {
-	document.getElementById(scrollToId).scrollIntoView({
+	const element = document.getElementById(scrollToId);
+	const offset = 85;
+	const bodyRect = document.body.getBoundingClientRect().top;
+	const elementRect = element.getBoundingClientRect().top;
+	const elementPosition = elementRect - bodyRect;
+	const offsetPosition = elementPosition - offset;
+	window.scrollTo({
+		top: offsetPosition,
 		behavior: "smooth",
 	});
 }
